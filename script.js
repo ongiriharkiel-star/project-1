f858e15cb12d6bf280702b64a1c9d5b72bbd9b48
harkiel <ongiriharkiel@gmail.com>
2026-06-19 01:06:28 +0300

    added dark mode toggle and counter

--- script.js
+++ script.js
@@ -7,4 +7,17 @@ btn.onclick = function() {
       btn.textContent = ON ? "Turn OFF" : "Turn ON";
         stat.textContent = ON ? "Status:ON" : "Status:OFF";
         stat.style.color = ON ? "green" : "red"; 
-    };
+document.body.style.backgroundColor = ON ?"#1a1a2e": "#f4f4f4";
+document.body.style.color = ON ?"#ffff" : "#222"};
+    const countDisplay = document.getElementById("count")
+const increaseBtn = document.getElementById("increase")
+const decreaseBtn = document.getElementById("decrease")
+let count = 0
+increaseBtn.onclick = function (){
+  count = count+1;
+  countDisplay.textContent=count};
+
+
+decreaseBtn.onclick = function(){
+  count =count-1;
+  countDisplay.textContent =count};
